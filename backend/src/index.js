809fb6f90e597baecb1043f945ebd50f8fcbce1e
Weverton Icaro <vertin_xd@hotmail.com>
2020-03-11 17:48:30 -0300

    Back-end
    Developer search application back-end

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://kosanage:a1l2o3n4e5@cluster0-shp93.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(2704);
